f6668617474859388ea53830e64ad413a44eea29
Melody <hi@melody.cool>
2018-10-10 17:17:23 -0400

    Javascript, begone

--- scripts/site.js
+++ scripts/site.js
@@ -1,17 +1,1 @@
 // Site scripts go here!
-
-// Smooth Scroll (https://css-tricks.com/snippets/jquery/smooth-scrolling/)
-$(function() {
-  $('a[href*="#"]:not([href="#"]):not(".image-container")').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
